60b53cd57a8a8edaf3a1df0fdfc9d1af3d26566b
muktadira22 <aryamuktadir@gmail.com>
2022-11-17 14:46:38 +0000

    Initial Commit

---
+++ comment/comment.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World") // Menampilkan Pesan Hello World
+	// fmt.Println("Baris ini akan diabaikan")
+
+	/*
+		ini komentar multi line
+	*/
+}
---
+++ variable/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Cara Pertama
+	var namadepan string = "Aria"
+
+	// Cara Kedua
+	var namabelakang = "Muktadir"
+
+	// Cara Ketiga
+	namatengah := "Ai"
+
+	// Mengubah Variable
+	namabelakang = "Ganteng"
+
+	fmt.Printf("Halo %s %s %s", namadepan, namatengah,namabelakang)
+}
