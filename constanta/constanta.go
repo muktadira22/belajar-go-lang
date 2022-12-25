a92eb4a8cb9a603466de5435570b13baa71e623a
muktadira22 <aryamuktadir@gmail.com>
2022-12-25 14:10:32 +0000

    Constanta

---
+++ constanta/constanta.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const x = 30
+
+	fmt.Printf("%d\n", x)
+
+}
---
+++ tipe-data/tipe-data.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	var number_positif uint16 = 256
+	fmt.Println(number_positif)
+}
